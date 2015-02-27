bec89b4c9cb7ea522567b438f1e414bdb7142890
garin <garinspring@163.com>
2015-02-27 14:23:06 +0800

    Initial Commit

---
+++ pp_new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pp_new
+//
+//  Created by GaoYong on 15/2/27.
+//  Copyright (c) 2015年 GaoYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pp_new/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pp_new
+//
+//  Created by GaoYong on 15/2/27.
+//  Copyright (c) 2015年 GaoYong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
