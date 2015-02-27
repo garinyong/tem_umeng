0f5b4c8ec0804d8d6d4bd6c08d4c2913d073cee6
garin <garinspring@163.com>
2015-02-27 17:39:56 +0800

    tem umeng

--- pp_new/ViewController.m
+++ pp_new/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UMFeedback.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn  =[UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor whiteColor];
+    btn.frame = CGRectMake(self.view.bounds.size.width/2 - 50, self.view.bounds.size.height/2 - 40 ,100, 80);
+    btn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [btn setTitle:@"feedback" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+-(void) btnClick
+{
+    [self presentViewController:[UMFeedback feedbackModalViewController] animated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
